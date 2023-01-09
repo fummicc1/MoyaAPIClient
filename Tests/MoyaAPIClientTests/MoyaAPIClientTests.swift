b491a1671c353d12e3938afc8965cea0f7483969
fummicc1 <fumiya.tennis1@gmail.com>
2023-01-10 02:41:49 +0900

    Add APIClient

--- Tests/MoyaAPIClientTests/MoyaAPIClientTests.swift
+++ Tests/MoyaAPIClientTests/MoyaAPIClientTests.swift
@@ -2,10 +2,4 @@ import XCTest
 @testable import MoyaAPIClient
 
 final class MoyaAPIClientTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(MoyaAPIClient().text, "Hello, World!")
-    }
 }
